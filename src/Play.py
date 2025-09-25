933f4a379a60937776eca176942efadd8d7ee86f
zero-Praveen <4praveencareer@gmail.com>
2025-09-25 17:10:52 +0530

    Initial commit

---
+++ src/Play.py
@@ -0,0 +1,3 @@
+nums=[1,2,2,3,4,4,5]
+unique_squares={x**2 for x in nums}
+print(unique_squares)
---
+++ src/__init__.py
@@ -0,0 +1,32 @@
+# import numpy as np
+
+# a = np.array([1,2,3])
+# b = np.array([[1,2], [3,4]])
+
+# print(a.mean())
+# print(b.shape)
+
+# import pandas as pd
+
+# df = pd.read_excel(r"C:\Users\4prav\my_project\data\raw\Sasa.xlsx", sheet_name='Sheet1')
+# print(df.head())
+# print(df.describe())
+
+# import matplotlib.pyplot as plt
+
+# plt.plot([1,2,3],[4,5,6])
+# plt.title("Simple Line Plot")
+
+# plt.xlabel("x axis")
+# plt.ylabel("y axis")
+# plt.show()
+
+# from sklearn.linear_model import LinearRegression 
+# from sklearn.model_selection import train_test_split
+
+# x =['feature1','feature2']
+# y =['target1', 'target2']
+
+# x_train, x_test, y_train, y_test = train_test_split(x,y)
+# model=LinearRegression()
+# model.fit(x_train, y_train)
